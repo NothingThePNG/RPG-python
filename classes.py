26425cd329d67b0efd8553fcf4ef4e185d658c2c
NothingThePNG <lukelionleo@gmail.com>
2024-07-12 16:21:52 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,45 @@
+from random import *
+from g_print import g_print_str
+
+class Creature:
+    def __init__(self, attributes) -> None:
+        self.name = attributes[0]
+        self.health = attributes[1]
+        self.armor = 1
+        self.regen = 20
+        self.melee = 5
+        self.enemys: list[Creature] = []
+    
+    def hurt(self, damage: int, attacker):
+        self.health -= damage
+
+        if attacker not in self.enemys:
+            self.enemys.append(attacker)
+
+    def attack(self, hit) -> None:
+        if len(self.enemys) <= 0:
+            return None
+        
+        if hit == None:
+            enemy: Creature = choice(self.enemys)
+        else:
+            enemy: Creature = self.enemys[hit]
+
+        enemy.hurt(self.melee, self)
+        g_print_str(f"{self} attacked {enemy} for {self.melee} damage")
+        g_print_str(f"{enemy} has {enemy.health}HP\n")
+
+        if enemy.health <= 0:
+            g_print_str(f"{enemy} has died to {self}")
+            self.enemys.remove(enemy)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Player(Creature):
+    def __init__(self) -> None:
+        super().__init__(["player", 100])
+
+    def heal(self) -> None:
+        self.health += self.regen
+        g_print_str(f"{self} healed for {self.regen}")
---
+++ main.py
@@ -0,0 +1,58 @@
+from g_print import *
+from get_type import *
+from random import *
+from time import sleep
+from classes import *
+
+creature_types = [["rat", 10], ["spider", 5]]
+
+player = Player()
+
+def make_enemys(min, max):
+    count = randint(min, max)
+    ret = []
+    for i in range(count):
+        ret.append(Creature(choice(creature_types)))
+
+    return ret
+
+def combat():
+    print(f"You have {player.health}HP")
+
+    hostile = make_enemys(1, 4)
+
+    g_print_str("You are attacked by:")
+    for i in hostile:
+        i.enemys.append(player)
+        g_print_str(f"    -   {i}")
+    
+    sleep(1)
+
+    print()
+
+    player.enemys = hostile
+
+    while len(hostile) > 0 and player.health > 0:
+        for i in hostile:
+            i.attack(None)
+            sleep(1)
+
+        action = get_int(first_out="Do you want to \n1: Heal \n2: Attack\n", output="\nDo you want to \n1: Heal \n2: Attack\n", vaid_range=[1, 3])
+
+        if action == 1:
+            player.heal()
+        elif action == 2:
+            attacking = get_int(output="Who do you want to attack?: ", vaid_range=[0, len(hostile)+1])-1
+
+            player.attack(attacking)
+        sleep(1)
+
+def play():
+    combat()
+
+
+def main():
+    if __name__ == "__main__":
+        play()
+
+main()
